3f9534c83f0271b420b4e8c03ef993bc0b20c24b
thomas <thonuck@gmail.com>
2022-12-08 22:22:43 +0100

    Initial Version with flat structure

---
+++ StromCounter.py
@@ -0,0 +1,24 @@
+
+from kivymd.app import MDApp
+import logging
+import datetime
+
+logger = logging.getLogger()
+
+class StromCounterApp(MDApp):
+    def callback(self):
+        logger.info("The callback was called!!!")
+
+    def get_date(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d")
+
+    def get_time(self):
+        return datetime.datetime.now().strftime("%H:%M")
+
+    def enter_data(self):
+        logger.info("Enter Data Called!")
+        app = self.get_running_app()
+        app.ids
+
+
+StromCounterApp().run()

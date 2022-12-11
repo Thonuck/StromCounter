06df37f4bd7cf937d86248d3fdf47e3a3fd4031b
thomas <thonuck@gmail.com>
2022-12-11 21:41:18 +0100

    Improvement of Screen manager

--- StromCounter.py
+++ StromCounter.py
@@ -4,10 +4,20 @@ import logging
 import datetime
 import EingabeScreen
 import TabellenScreen
+from kivy.lang import Builder
+from kivymd.uix.navigationdrawer import MDNavigationLayout
 
 logger = logging.getLogger()
 
+class ApplicationLayout(MDNavigationLayout):
+    pass
+
 class StromCounterApp(MDApp):
+    
+    def build(self):
+        self.app_layout = ApplicationLayout()
+        return self.app_layout
+
     def callback(self):
         logger.info("The callback was called!!!")
 
@@ -19,8 +29,9 @@ class StromCounterApp(MDApp):
 
     def enter_data(self):
         logger.info("Enter Data Called!")
-        app = self.get_running_app()
-        app.ids
+        print(dir(self.app_layout.nested_sm))
+        print("Screen Names: {}".format(self.app_layout.nested_sm.screen_names))
+        self.app_layout.nested_sm.current = "Anschauen"
 
 
 StromCounterApp().run()
